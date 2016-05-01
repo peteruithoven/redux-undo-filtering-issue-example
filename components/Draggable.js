042fc2fb8ee03ae0cc39d9a4f6af8f13e3ba2365
peteruithoven <peter@peteruithoven.nl>
2016-05-01 11:51:17 +0200

    Moving store out of draggable

--- components/Draggable.js
+++ components/Draggable.js
@@ -3,14 +3,10 @@ import { bindActionCreators } from 'redux'
 import 'pepjs'
 
 export default class Draggable {
-  constructor (elem, store) {
+  constructor (elem, dispatch) {
     console.log('Draggable');
     this.elem = elem
-    this.actions = bindActionCreators(actions, store.dispatch)
-    this.update(store.getState())
-    store.subscribe(() => {
-      this.update(store.getState())
-    })
+    this.actions = bindActionCreators(actions, dispatch)
 
     let dragging = false
     this.elem.addEventListener('pointerdown', (event) => {
@@ -30,7 +26,7 @@ export default class Draggable {
     })
   }
   update (state) {
-    const {x, y, dragging} = state.draggable.present
+    const {x, y, dragging} = state;
     this.elem.style.left = `${x}px`
     this.elem.style.top = `${y}px`
     this.elem.className = dragging ? 'dragging' : '';
--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const store = createStore(reducers)
 
 const draggable = new Draggable(
   document.getElementById('draggable'),
-  store
+  store.dispatch
 )
 
 const undoButton = document.getElementById('undoButton')
 undoButton.addEventListener('click', (event) => {
   store.dispatch(UndoActionCreators.undo())
 })
+
+function update() {
+  const state = store.getState();
+  draggable.update(state.draggable.present);
+}
+store.subscribe(update);
+update()
